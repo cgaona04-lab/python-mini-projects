d0e86817da7d2230d7dd7b7a797adcd86ca00c84
cgaona04-lab <cgaona04@gmail.com>
2026-07-18 16:24:12 -0500

    Update mad-libs.py

--- mad-libs.py
+++ mad-libs.py
@@ -1,1 +1,4 @@
-
+noun = input("Type a noun: ")
+verb = input("Type a verb in the past tense: ")
+adjective = input("Type an adjective: ")
+print(f"The {adjective} teacher {verb} a {noun} during class.")
